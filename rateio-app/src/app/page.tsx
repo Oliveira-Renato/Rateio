81eb8b2b6c43b6c878456925029cc6e8259687cc
Renato <renato.printf@gmail.com>
2023-10-27 15:51:47 -0300

    front in progress

--- rateio-app/src/app/page.tsx
+++ rateio-app/src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Rateio App</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* left */}
+      <div>
+        left
+      </div>
+      {/* right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            No momento, não há divisões registradas. Comece a{' '}
+            <a className="underline hover:text-gray-50 transition-all" href="">adicionar despesas</a> para iniciar a divisão com seus amigos.
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
