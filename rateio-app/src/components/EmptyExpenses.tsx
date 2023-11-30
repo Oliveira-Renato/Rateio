4e057e7f3aff4c287c4a763297758416a7e1001e
Renato <renato.printf@gmail.com>
2023-11-30 04:20:20 -0300

    empty expenses

--- rateio-app/src/components/EmptyExpenses.tsx
+++ rateio-app/src/components/EmptyExpenses.tsx
@@ -1,7 +1,51 @@
+'use client'
+import axios from "axios";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
+interface GroupProps {
+  id: string,
+  name: string
+}
 
 export default function EmptyExpenses() {
+  const [groupName, setGroupName] = useState<GroupProps[]>()
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const getData = async () => {
+      if (session) {
+        try {
+          const response = await axios.get(`http://localhost:3333/user/${session.googleId}`)
+
+          if (response.data) {
+            const { data } = response
+            setGroupName(data.groups)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    getData()
+  }, [session?.googleId])
+
+  if (groupName) {
+    return (
+      <div>
+        {groupName.map((group, index) => (
+          <div key={index}>
+            <div>
+              <h5>{group.name}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+
   return (
     <div className="flex flex-1 items-center justify-center">
       <p className="text-center leading-relaxed w-[360px]">
@@ -10,4 +54,5 @@ export default function EmptyExpenses() {
       </p>
     </div>
   )
+
 }
