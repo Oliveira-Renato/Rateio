44c39a2e1d581ab6a934cdabe395b7cfe01d666b
Renato <renato.printf@gmail.com>
2023-12-04 14:49:49 -0300

    fix

--- rateio-app/src/components/Hero.tsx
+++ rateio-app/src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div>
       {/* <Image src={ } alt='Bla' /> */}
       <div className='container mx-auto text-center py-20'>
-        <h1 className='text-4xl font-bold mb-4'>Simplificando a Divisão de Despesas em Viagens em Grupo</h1>
+        <h1 className='text-4xl font-bold mb-4'>Simplificando a Divisão de Despesas em Viagens em Grupo 💸</h1>
 
         <p className='text-lg mb-8'>
           Organize suas viagens com amigos de forma fácil e justa. Divida despesas, mantenha a transparência e foque nas memórias.
