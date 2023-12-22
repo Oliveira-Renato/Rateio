a1ac140674707301724530aa4d910a00a65a1194
Renato <renato.printf@gmail.com>
2023-12-22 16:32:12 -0300

    fix scroll

--- rateio-app/src/components/MainButton.tsx
+++ rateio-app/src/components/MainButton.tsx
@@ -1,12 +1,37 @@
 'use client'
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function MainButton() {
   const scrollRef = useRef<HTMLAnchorElement | null>(null);
   const { data: session } = useSession();
   const router = useRouter();
+
+  const handleButtonClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    router.push('/new/group');
+
+    if (window.innerWidth < 960 && scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 960 && scrollRef.current) {
+        scrollRef.current.scrollIntoView({ behavior: 'auto' });
+      }
+    };
+
+    // Adiciona um ouvinte de evento de redimensionamento para tratar a mudança de tamanho da tela
+    window.addEventListener('resize', handleResize);
+
+    // Remove o ouvinte de evento ao desmontar o componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   if (!session) {
     return (
@@ -24,13 +49,7 @@ export default function MainButton() {
     <div>
       <a
         className='bg-orange-500 text-white px-6 py-3 rounded hover:bg-orange-700 transition duration-300 ease-in-out cursor-pointer'
-        onClick={(e) => {
-          e.preventDefault();
-          router.push('/new/group'); // Substitua 'sua-nova-rota' pela rota desejada
-          if (scrollRef.current) {
-            scrollRef.current.scrollIntoView({ behavior: 'smooth' });
-          }
-        }}
+        onClick={handleButtonClick}
         ref={scrollRef}
       >
         Criar Nova Despesa
