d4ca31522cc5b2c98894c0da43a29bd4b4cb339e
Oliveira-Renato <renato.pritf@gmail.com>
2024-07-04 17:51:53 -0300

    fix 8

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,10 +1,10 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors'
 import serverless from 'serverless-http'
-import { userRoutes } from './src/routes/users';
-import { groupsRoutes } from './src/routes/groups'
-import { participantsRoutes } from './src/routes/participants'
-import { registerUser } from './src/routes/register'
+import { userRoutes } from '../src/routes/users';
+import { groupsRoutes } from '../src/routes/groups'
+import { participantsRoutes } from '../src/routes/participants'
+import { registerUser } from '../src/routes/register'
 
 const app = fastify({ logger: true });
 
